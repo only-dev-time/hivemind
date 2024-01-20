b68ab688effe9d653693be6a68c30b1757d0e735
only-dev-time <111915936+only-dev-time@users.noreply.github.com>
2024-01-20 22:42:25 +0100

    Add foreign key constraints hive_bookmarks table

--- hive/db/schema.py
+++ hive/db/schema.py
@@ -364,10 +364,14 @@ def build_metadata_bookmarks(metadata=None):
 
     sa.Table(
         'hive_bookmarks', metadata,
-        sa.Column('id', sa.Integer, primary_key=True, autoincrement=True),
+        sa.Column('id', sa.Integer, primary_key=True, autoincrement=True), # TODO notwendig?
         sa.Column('account_id', sa.Integer, nullable=False),
         sa.Column('post_id', sa.Integer, nullable=False),
         sa.Column('bookmarked_at', sa.DateTime, nullable=False),
+        
+        sa.ForeignKeyConstraint(['account_id'], ['hive_accounts.id'], name='hive_bookmarks_fk1'),
+        sa.ForeignKeyConstraint(['post_id'], ['hive_posts.id'], name='hive_bookmarks_fk2'),
+        
         sa.UniqueConstraint('account_id', 'post_id', name='hive_bookmarks_ux1'),
         sa.Index('hive_bookmarks_ix1', 'account_id'), # bookmarks from account
         sa.Index('hive_bookmarks_ix2', 'post_id'), # bookmarks for a post
