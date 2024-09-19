1afb0014cc6810dd37f29a3b4e4434643d62243b
only-dev-time <111915936+only-dev-time@users.noreply.github.com>
2024-09-19 22:56:37 +0200

    new score with median rshare

--- hive/utils/post.py
+++ hive/utils/post.py
@@ -196,7 +196,7 @@ def post_payout(post):
     # trending scores
     _timestamp = utc_timestamp(parse_time(post['created']))
     # test new score without bot votes
-    sc_trend = _score_without_bots(post['active_votes'], _timestamp, 240000)
+    sc_trend = _score_with_median_rshares(post['active_votes'], _timestamp, 240000)
     # sc_trend = _score(rshares, _timestamp, 240000)
     sc_hot = _score(rshares, _timestamp, 10000)
 
@@ -256,6 +256,12 @@ def _score_without_bots(active_votes, created_timestamp, timescale=480000):
         no_bot_rshares += int(vote["rshares"]) if vote["voter"] not in BOTS else 0
     
     return _score(no_bot_rshares, created_timestamp, timescale)
+
+def _score_with_median_rshares(active_votes, created_timestamp, timescale=480000):
+    """Calculate trending/hot score with median rshares."""
+    rshares = [int(vote['rshares']) for vote in active_votes]
+    median_rshare = sorted(rshares)[len(rshares) // 2]
+    return _score(median_rshare, created_timestamp, timescale)
 
 def post_stats(post):
     """Get post statistics and derived properties.
